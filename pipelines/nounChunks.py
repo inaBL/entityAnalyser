2bd9ebbb42f79aa197574f412914c91632d45406
iBL <ina.liukkonen@gmail.com>
2019-10-15 14:53:05 +0300

    Added root word sentiment functions

--- pipelines/nounChunks.py
+++ pipelines/nounChunks.py
@@ -4,12 +4,13 @@ from timeit import default_timer as timer
 from datetime import datetime
 from sentiment import classSentiment
 
+# TBI: pandas dataframe compatibility
 nlp = spacy.load('en_core_web_lg')
 s = classSentiment.Sentiment
 lemma = Lemmatizer()
 
 
-# Returns list of noun chunk and root text
+# Returns list of noun chunk and root word text
 def noun_chunks_list(document) -> list:
     chunks = [(item.text, item.root.text) for item in document.noun_chunks]
     return chunks
@@ -28,7 +29,7 @@ def noun_chunks_list_cleaned(document) -> list:
     return chunks
 
 
-# Without root word
+# Returns list of noun chunk text, without the root word
 def noun_chunks_min(document) -> list:
     chunks = []
 
@@ -67,8 +68,39 @@ def noun_chunks_sentiment(document) -> list:
     return chunk_sentiment
 
 
-def noun_chunks_root_sentiment(document) -> dict:
-    return {}
+# Returns list of tuples, Tuple[1] contains the sentiment values of tokens in the noun chunk, Tuple[2] is the root word.
+# E.g. ([1, 0], 'decay')
+def noun_chunks_roots_sentiment(document) -> list:
+    chunk_sentiment = []
+
+    for item in document.noun_chunks:
+        chunk = []
+        for word in item.text.split():
+            chunk.append(s.sentiment_single_word(lemma.lookup(word)))
+        chunk_sentiment.append((chunk, item.root.text))
+
+    return chunk_sentiment
+
+
+# Returns dictionary, with the root word as key and list of the chunk token sentiment values related to the key
+# E.g. 'decay': [1, 0]
+# Calls function noun_chunks_roots_sentiment()
+def noun_chunks_rootword_sentiment(document) -> dict:
+    roots_dict = {}
+    sentiment, roots = zip(*noun_chunks_roots_sentiment(document))
+    l_roots = [token.lower() for token in roots]
+
+    l_sentiment = list(zip(l_roots, sentiment))
+
+    for item in l_sentiment:
+        sentiments = item[1]
+        if item[0] not in roots_dict.keys():
+            roots_dict[item[0]] = sentiments
+        elif item[0] in roots_dict.keys():
+            sentiments = item[1] + roots_dict[item[0]]
+            roots_dict[item[0]] = sentiments
+
+    return roots_dict
 
 
 if __name__ == '__main__':
@@ -83,5 +115,5 @@ if __name__ == '__main__':
     print(f'Finished text to document import at {datetime.now()}. '
           f'\nTook {end - start} seconds')
 
-    print(noun_chunks_sentiment(doc))
+    print(noun_chunks_roots_sentiment(doc))
 
