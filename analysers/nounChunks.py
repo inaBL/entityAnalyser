3f4ed2da6f4cada9c500ebedda98ac139b60cd35
iBL <ina.liukkonen@gmail.com>
2019-10-30 12:18:14 +0200

    Added lemma clustering for NC

--- analysers/nounChunks.py
+++ analysers/nounChunks.py
@@ -1,3 +1,14 @@
+'''
+Functions for extracting noun-chunk information from text files.
+If run directly, writes a csv file at specified location.
+
+En_core_web_lg may take time to load depending on your set up, consider downloading and using sm or md versions as needed.
+
+NOTE: Document to doc conversion takes approx 0,4 ms per token.
+
+INPUT: Text for spacy document object.
+OUTPUT: Basic stats in csv file.
+'''
 import spacy
 from spacy.lemmatizer import Lemmatizer
 from timeit import default_timer as timer
@@ -45,8 +56,8 @@ def nc_min(document) -> list:
     return chunks
 
 
-# Returns list of tuples with noun chunk split into words and root word.
-# Tuple[1] being token list and Tuple[2] the rootword.
+# Returns list of tuples with noun chunk split into words and root word
+# Tuple[1] being token list and Tuple[2] the rootword
 # E.g (['swift', 'decay'], 'decay')
 def nc_words(document) -> list:
     words = []
@@ -57,11 +68,41 @@ def nc_words(document) -> list:
     return words
 
 
-# Returns dictionary of all roots as keys, and all unique noun chunk tokens related to the root as list
+# Returns list of tuples with noun chunk split into lemmas and lemmatised root word in lowercase
+def nc_lemmas(document) -> list:
+    words = []
+
+    for item in document.noun_chunks:
+        chunk = []
+
+        for word in item.text.replace('\n', '').split():
+            chunk.append(lemma.lookup(word).lower())
+
+        words.append((chunk, lemma.lookup(item.root.text.lower())))
+
+    return words
+
+
+# Returns dictionary of all roots as keys + all unique noun chunk tokens related to the root as list
 def nc_clusters(document) -> dict:
     clusters = {}
 
     for item in nc_words(document):
+        words = list(set(item[0]))
+        if item[1] not in clusters.keys():
+            clusters[item[1]] = words
+        elif item[1] in clusters.keys():
+            words = list(set(item[0] + clusters[item[1]]))
+            clusters[item[1]] = words
+
+    return clusters
+
+
+# Returns dictionary of all unique lemmatised roots as keys + all unique noun chunk lemmas related to the root as list
+def nc_lemma_clusters(document) -> dict:
+    clusters = {}
+
+    for item in nc_lemmas(document):
         words = list(set(item[0]))
         if item[1] not in clusters.keys():
             clusters[item[1]] = words
@@ -148,6 +189,25 @@ def nc_root_mostneg(document, number=10):
     return dict(sorted(nc_root_sentiment_score(document).items(), key=lambda x: x[1])[:number])
 
 
+# Return list of tokens of noun clusters related to given word
+# If the word doesn't appear as root, return empty list
+def nc_word_cluster(word: str, document) -> list:
+    if word in nc_clusters(document).keys():
+        return nc_clusters(document)[word]
+    else:
+        return []
+
+
+# Return list of lemmas of noun clusters related to lemmatised given word
+# If the word doesn't appear as root, return empty list
+def nc_word_lemmas(word: str, document) -> list:
+    lemmatised = lemma.lookup(word).lower()
+    if lemmatised in nc_lemma_clusters(document).keys():
+        return nc_lemma_clusters(document)[lemmatised]
+    else:
+        return []
+
+
 if __name__ == '__main__':
     # Set document to be analysed below
     print(f'Starting text to document import at {datetime.now()} ...')
@@ -160,5 +220,5 @@ if __name__ == '__main__':
     print(f'Finished text to document import at {datetime.now()}. '
           f'\nTook {end - start} seconds')
 
-    print(nc_clusters(doc))
+    print(nc_word_lemmas('dish', doc))
 
